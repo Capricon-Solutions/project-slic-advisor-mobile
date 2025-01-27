2ec0c1f15ce633f9ea47fcc9409bc0ab5d2db18a
lakith Wijenayaka <lakithwijenayaka.dev@gmail.com>
2025-01-27 17:08:54 +0530

    splash screen

--- App.tsx
+++ App.tsx
@@ -5,9 +5,10 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from 'react-native-splash-screen'
 
 import {
   StyleSheet,
@@ -41,6 +42,12 @@ const DetailsScreen = () => {
 const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
